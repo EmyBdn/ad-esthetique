08c37c9966b4b43fffd6001fca77b6784bcd732c
BAUDOUIN Emeline <emeline.baudouin@suivideflotte.net>
2026-06-22 09:41:43 +0200

    log out button admin

--- src/components/admin/HeaderAdmin.tsx
+++ src/components/admin/HeaderAdmin.tsx
@@ -1,9 +1,12 @@
+import LogoutButton from "@/components/LogOutButton";
+
 export default function AdminHeader() {
   return (
-    <header className="p-6 border-b bg-white">
+    <header className="p-6 border-b bg-white flex items-center justify-between">
       <h1 className="text-xl font-bold text-slate-800">
         Panneau d'administration
       </h1>
+      <LogoutButton />
     </header>
   );
 }
